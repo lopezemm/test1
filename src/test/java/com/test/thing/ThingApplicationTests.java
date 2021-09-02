72f843e5aab4eb233a693b7c89507467246065ea
Emmanuel de Jesus Lopez Rincon <emmanuel.lopez@unosquare.com>
2021-09-02 11:41:15 -0500

    first commit

---
+++ src/test/java/com/test/thing/ThingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.thing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
